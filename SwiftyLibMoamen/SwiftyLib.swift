953d28eb2ed273c72caf029dedd2f9a4952ff208
Moamenzalabia <moamenzalabia@gmail.com>
2021-10-05 12:54:02 +0200

    Added test cases

---
+++ SwiftyLibMoamen/SwiftyLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLibMoamen
+//
+//  Created by Moamen Abd Elgawad on 05/10/2021.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
