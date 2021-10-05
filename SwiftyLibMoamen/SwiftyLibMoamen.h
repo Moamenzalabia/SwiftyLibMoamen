8306281b6e77c5b4ccaf0fdb2662b67dde2eaed3
Moamenzalabia <moamenzalabia@gmail.com>
2021-10-05 12:41:18 +0200

    first commit

---
+++ SwiftyLibMoamen/SwiftyLibMoamen.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyLibMoamen.h
+//  SwiftyLibMoamen
+//
+//  Created by Moamen Abd Elgawad on 05/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLibMoamen.
+FOUNDATION_EXPORT double SwiftyLibMoamenVersionNumber;
+
+//! Project version string for SwiftyLibMoamen.
+FOUNDATION_EXPORT const unsigned char SwiftyLibMoamenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibMoamen/PublicHeader.h>
+
+
